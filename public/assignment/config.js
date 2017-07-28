0c335a675d852f4997fdd2f63cc82d9a056cfe2f
Lucy Linderman <linderman.l@husky.neu.edu>
2017-07-28 10:54:08 -0700

    routing

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -4,7 +4,7 @@
         .config(Config);
     function Config($routeProvider, $locationProvider) {
         $routeProvider
-            .when("login", {
+            .when("/login", {
                 templateUrl: "views/user/templates/login.view.client.html",
                 controller: "LoginController",
                 controllerAs: "model"
