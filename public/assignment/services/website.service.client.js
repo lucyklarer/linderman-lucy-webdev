a98a0b47742b19c6e98482d56514424b0b29d0c9
Lucy Linderman <linderman.l@husky.neu.edu>
2017-08-04 14:21:42 -0700

    debugging

--- public/assignment/services/website.service.client.js
+++ public/assignment/services/website.service.client.js
@@ -5,13 +5,13 @@
     function WebsiteService() {
         var websites =
             [
-                { _id: "123", name: "Facebook",    developerId: "456", description: "Lorem" },
-                { _id: "234", name: "Twitter",     developerId: "456", description: "Lorem" },
-                { _id: "456", name: "Gizmodo",     developerId: "456", description: "Lorem" },
-                { _id: "890", name: "Go",          developerId: "123", description: "Lorem" },
-                { _id: "567", name: "Tic Tac Toe", developerId: "123", description: "Lorem" },
-                { _id: "678", name: "Checkers",    developerId: "123", description: "Lorem" },
-                { _id: "789", name: "Chess",       developerId: "234", description: "Lorem" }
+                { "_id":"123", "name":"Facebook", "developerId":"456", "description":"Lorem" },
+                { "_id": "234", "name": "Twitter",     "developerId": "456", "description": "Lorem" },
+                { "_id": "456", "name": "Gizmodo",     "developerId": "456", "description": "Lorem" },
+                { "_id": "890", "name": "Go",          "developerId": "123", "description": "Lorem" },
+                { "_id": "567", "name": "Tic Tac Toe", "developerId": "123", "description": "Lorem" },
+                { "_id": "678", "name": "Checkers",    "developerId": "123", "description": "Lorem" },
+                { "_id": "789", "name": "Chess",       "developerId": "234", "description": "Lorem" }
             ];
         var api = {
             "createWebsite"   : createWebsite,
@@ -30,6 +30,7 @@
         function findWebsitesByUser(userId) {
             var i;
             var userWebsites;
+            userWebsites = null;
             for (i = 0; i < websites.length; i++) {
                 if (websites[i].developerId === userId) {
                     log.console("found website " + websites[i].name);
