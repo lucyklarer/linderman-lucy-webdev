401b30b1e158dbfe872588c88d657e99f2753be4
Lucy Linderman <linderman.l@husky.neu.edu>
2017-08-28 13:03:06 -0700

    youtube widget source

--- public/assignment/services/widget.service.client.js
+++ public/assignment/services/widget.service.client.js
@@ -11,7 +11,7 @@
                 { "_id": "456", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"},
                 { "_id": "567", "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
                 { "_id": "678", "widgetType": "YOUTUBE", "pageId": "321", "width": "100%",
-                    "url": "https://youtu.be/AM2Ivdi9c4E" },
+                    "url": "https://www.youtube.com/embed/Ndlf5_L5gsE" },
                 { "_id": "789", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"}
             ];
         var api = {
