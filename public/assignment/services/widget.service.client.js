9a552fa3731386c36ecdf207a9b9aaf9361a33f2
Lucy Linderman <linderman.l@husky.neu.edu>
2017-09-07 14:43:58 -0700

    widget list scrolling

--- public/assignment/services/widget.service.client.js
+++ public/assignment/services/widget.service.client.js
@@ -5,6 +5,8 @@
     function WidgetService() {
         var widgets =
             [
+                { "_id": "111", "widgetType": "YOUTUBE", "pageId": "321", "width": "100%",
+                    "url": "https://www.youtube.com/embed/Ndlf5_L5gsE" },
                 {"_id": "123", "widgetType": "HEADING", "pageId": "321", "size": 2, "text": "GIZMODO"},
                 {"_id": "234", "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
                 { "_id": "345", "widgetType": "IMAGE", "pageId": "321", "width": "100%", "url": "http://lorempixel.com/400/200/"},
